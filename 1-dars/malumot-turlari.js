05bd1a401a6f60957ef84c721f994a7dd7f27759
Islomkhudja <nanotexnolagiya@gmail.com>
2021-08-17 22:54:54 +0500

    Ozgaruvchilar va malumot turlari

---
+++ 1-dars/malumot-turlari.js
@@ -0,0 +1,58 @@
+// O'zgaruvchilar
+
+// let name = 'Islom';
+// var age = 12;
+
+// Harflar sonlar va 2ta $ va _
+// let, function, var, delete, return, break, if
+// Son bilan boshlanishi kerak emas
+// camelCase
+
+// Ma'lumot turlari
+
+// 1-ma'lumot turi number - sonlar
+// +/- 2\53 - 1
+let hour = 1.1
+console.log(11);
+
+// 2- ma'lumot turi - BigInt
+// 100n
+
+// 3-ma'lumot turi - string
+let name = 'Mening ismim Islom';
+let name2 = "Ikkinchi ism";
+let name3 = `Salom ${name2}`;
+console.log(name3)
+
+// 4-ma'lumot turi - boolean true / false
+let isNumber = true
+
+// 5-ma'lumot turi - object
+// object-da kalit faqat string | number | symbol
+let faydalanuvchi = {
+  nomi: 'Islom',
+  yoshi: 25,
+}
+
+// 6-ma'lumot turi
+// Symbol
+let key = Symbol('kalit');
+
+// 7-ma'lumot turi
+// null -> Hech narsa yoq yoki bosh bosh
+let user = null;
+
+// 8-ma'lumot turi
+// undefined -> Biriktirilmagan
+let variables = undefined
+
+user = "Anvar";
+
+console.log(typeof user)
+
+
+// Constants
+const LIMIT = 12;
+
+// let, var, const
+// 12.1.1 version
