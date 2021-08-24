3c63a544d774d042761a5a0e1d2d7978e7b21467
abdurasul18 <abdurasulhafizov3@gmail.com>
2021-08-24 09:52:34 +0500

    uyga vzifa-4 fixed

--- uyga-vazifa/4-dars/abdurasul-vazifa-1.js
+++ uyga-vazifa/4-dars/abdurasul-vazifa-1.js
@@ -1,39 +1,35 @@
-function romanToArabic(romStr) {
-  let romArr = romStr.toLocaleUpperCase().split("");
-  let arabArr = romArr.map((el) => letterConvertor(el));
-  if (arabArr.some((el) => isNaN(el))) return NaN;
-  return arabicFromArr(arabArr);
+function romanToArabic(roman){
+  if(roman == null)
+      return -1;
+  var totalValue = 0, 
+      value = 0, 
+      prev = 0;
+  for(var i=0;i<roman.length;i++){
+      var current = char_to_int(roman.charAt(i));
+      if (current > prev) {
+          totalValue -= 2 * value;
+      }
+      if (current !== prev) { 
+          value = 0; 
+      }
+      value += current;
+      totalValue += current;
+      prev = current;
+  }
+  return totalValue;
 }
-function arabicFromArr(arr) {
-  let max = Math.max.apply(null, arr);
-  let maxIndex = arr.indexOf(max);
-  let resultArr = arr.map((el, index) => {
-    if (index < maxIndex) return -1 * el;
-    return el;
-  });
-  return resultArr.reduce((accum, el) => accum + el);
-}
-function letterConvertor(letter) {
-  switch (letter) {
-    case "I":
-      return 1;
-    case "V":
-      return 5;
-    case "X":
-      return 10;
-    case "L":
-      return 50;
-    case "C":
-      return 100;
-    case "D":
-      return 500;
-    case "M":
-      return 1000;
-    default:
-      return NaN;
+
+function char_to_int(character) {
+  switch(character){
+      case 'I': return 1;
+      case 'V': return 5;
+      case 'X': return 10;
+      case 'L': return 50;
+      case 'C': return 100;
+      case 'D': return 500;
+      case 'M': return 1000;
+      default: return -1;
   }
 }
-console.log(romanToArabic("MMMDCCXXIV"));
-console.log(romanToArabic("CMM"));
-console.log(romanToArabic("XXI"));
-console.log(romanToArabic("XXCLXI"));
+
+console.log(romanToArabic('XXML'));
