72cc3f06e19d2a5c0db2998357c63186cd64e990
IslomQurbonov <islom_uz@inbox.ru>
2021-09-14 16:36:36 +0500

    9-dars

---
+++ 9-dars/strings.js
@@ -0,0 +1,59 @@
+let __hello = "hello"
+
+function calHello() {
+  return "Hello"
+}
+// let str = `${calHello()} Dev Quiz`
+
+function myTag(strings, nameExp, ageExp) {
+
+  let str0 = strings[0]
+  let str1 = strings[1]
+  let str2 = strings[2]
+
+  return `${str0}${nameExp}${str1}${age}${str2}`
+
+}
+
+let name = "Bahodir";
+let age = 18
+
+const output = myTag`Do'stim ${name}ning yoshi ${age} da`
+
+// console.log(output);
+
+
+let str2 = `Hello
+World`
+
+let charAt = str2.charAt(1000) // ""
+let index = str2[1000] // undefined
+
+
+// console.log(index.toLocaleLowerCase())
+
+for (let char of "DevQuiz") {
+  // console.log(char);
+}
+
+
+// toLowercase(), toUpperCase()
+
+
+// console.log(str.indexOf("JS", 2)) 
+// console.log(str.lastIndexOf("JS")) 
+
+// includes, startsWith, endsWith 
+// console.log(str.includes("Wel", 4)) 
+// console.log(str.startsWith("JS")) 
+// console.log(str.endsWith("JS+")) 
+
+// slice, substring, substr
+let str = "jS Welcome to JS+"
+
+console.log(String.fromCodePoint(119))
+
+
+
+
+
