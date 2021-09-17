ec2e4f4dd095c79809cbc69e4606af59d6ffa266
Jahongir7 <Jahongir95119@gmail.com>
2021-09-17 12:15:06 +0500

    7-vazifa ishlandi

--- uyga-vazifa/7-dars/Jahongir/7-vazifa.js
+++ uyga-vazifa/7-dars/Jahongir/7-vazifa.js
@@ -1,6 +1,13 @@
 function User(a, b) {
   this.qoshish = a + b;
   this.ayirish = a - b;
+  this.kopaytirish = a * b;
+  this.bolish = a / b;
+  this[Symbol.toPrimitive] = function (hint) {
+    if (hint === "string") return `${a},${b}`;
+    else if (hint === "number") return a > b ? a : b;
+    else return this;
+  };
 }
-const qoshish = new User(3, 4);
-console.log(qoshish.ayirish);
+
+module.exports = User;
