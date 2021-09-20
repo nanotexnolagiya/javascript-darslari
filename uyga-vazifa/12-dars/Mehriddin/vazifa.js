7e45e458ff16eb13892223f8630152dfafa99451
Mehriddin <mehriddinnozimov05@gmail.com>
2021-09-20 07:50:36 +0500

    v12 bajarildi

---
+++ uyga-vazifa/12-dars/Mehriddin/vazifa.js
@@ -0,0 +1,24 @@
+const {deepClone} = require("../../6-dars/Mehriddin/vazifa-Mehriddin")
+
+// savol obj.arr[0] bo`lganda nima qilish kerak?
+function changeArrInd(array){
+    array = new Proxy(array, {
+        get: function (arr, i) {
+            i < 0 ? res = arr.length + Number(i) : res = i - 1
+            return arr[res];
+        }
+    });
+    return array
+}
+
+
+function extendConstructor(child, parent){
+    child = deepClone(child)
+    child.__proto__ = parent
+    return child
+}
+
+module.exports = {
+    extendConstructor,
+    changeArrInd
+}
